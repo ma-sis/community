a2d9f28c9fc8bd04f40f05a8a29d3087a8da48b1
sohee <sohee@DESKTOP-7P7MT7G>
2019-08-25 16:27:11 +0900

    Style:Change css Feat:Add MessageBoard

---
+++ Community/src/main/java/org/masis/community/messageboard/DAO/MessageBoardDAO.java
@@ -0,0 +1,5 @@
+package org.masis.community.messageboard.DAO;
+
+public interface MessageBoardDAO {
+
+}
---
+++ Community/src/main/java/org/masis/community/messageboard/DAO/MessageBoardImpl.java
@@ -0,0 +1,8 @@
+package org.masis.community.messageboard.DAO;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MessageBoardImpl implements MessageBoardDAO {
+
+}
